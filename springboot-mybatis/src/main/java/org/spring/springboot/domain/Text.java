66581fed6abc2615cc1461403331c68fc906b2c9
cailinlin <kakamessiya@163.com>
2021-01-16 14:44:32 +0800

    将项目在centos中运行

---
+++ springboot-mybatis/src/main/java/org/spring/springboot/domain/Text.java
@@ -0,0 +1,16 @@
+package org.spring.springboot.domain;
+
+public class Text{
+    private String word;
+
+    public void setWord(String Word){
+        this.word = Word;
+
+    }
+
+    public void printWord(){
+        System.out.println(this.word);
+
+    }
+
+}
